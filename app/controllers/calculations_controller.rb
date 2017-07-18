08467034d04bed30b2cc74fe180619ad2f61b925
Saud <qahmad@chicagobooth.edu>
2017-07-18 12:34:23 +0000

    Part I First Attempt

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,57 @@
+class CalculationsController < ApplicationController
+   
+   def flex_square
+      
+    # The incoming parameters for this action look like {"a_number"=>"5"}
+    # Rails stores that hash in a variable called params
+    
+      @user_number = params["a_number"].to_i
+      @squared_number = @user_number ** 2
+      
+      render ("calculations/flexible_square_template.html.erb")
+   end
+   
+    def flex_squareroot
+      
+      @user_number = params["a_number"].to_f
+      @square_root = @user_number ** (1.0/2.0)
+      
+      render ("calculations/flexible_squareroot_template.html.erb")
+    end
+   
+    def flex_payment
+      
+      @term = params["term"].to_i
+      @principal = params["principal"].to_f      
+      @interest_rate = params["interest_rate"].to_f
+      
+      @interest_rate_decimal = @interest_rate/120000.0
+      @monthly_payment = (@interest_rate_decimal/(1-(1+@interest_rate_decimal)**(-1*@term*12)))*@principal
+      
+      render ("calculations/flexible_payment_template.html.erb")
+    end
+   
+    def flex_random
+      
+      @lower_limit = params["lower_limit"].to_i
+      @upper_limit = params["upper_limit"].to_i      
+      @random_number = rand(@upper_limit - @lower_limit) + @lower_limit
+      
+      render ("calculations/flexible_random_template.html.erb")
+    end
+   
+#############################################################################   
+   
+    def square_form
+      render ("calculations/square_form_template.html.erb")
+    end
+   
+    def process_square
+    
+        @user_number = params["user_number"].to_i
+        @squared_number = @user_number ** 2
+      
+      render ("calculations/square_results_template.html.erb")
+    end
+   
+end
